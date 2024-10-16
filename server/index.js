a69249f5c0b713e8c9e4331201deb781d722dede
amisha848 <samal.amisha2003@gmail.com>
2024-10-16 21:40:07 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./database/db.js";
+
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Server is working");
+  });
+
+import userRoutes from "./routes/user.js";
+
+app.use('/api',userRoutes);
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+    connectDb();
+  });
